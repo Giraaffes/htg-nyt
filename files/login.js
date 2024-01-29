0e3468d6c5ee69e4f0b24315af0c7cbee0218327
Marcus <marcusfaerk@gmail.com>
2024-01-29 17:23:05 +0100

    woopdeedoopdeedoo

--- files/login.js
+++ files/login.js
@@ -3,6 +3,6 @@ if (url.searchParams.has("backTo")) {
 	url.searchParams.delete("backTo");
 	location.replace(url);
 }
-
+ 
 $("input[name=\"query\"]").attr("placeholder", "E-MAIL");
-$("input[type=\"submit\"]").val("LOG IND"); 
+$("input[type=\"submit\"]").val("LOG IND");
