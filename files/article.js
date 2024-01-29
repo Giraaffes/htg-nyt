6dec0a17ae4543d4df323407604dff6274e531b1
Marcus <marcusfaerk@gmail.com>
2024-01-29 17:55:01 +0100

    fixed mobile login and article page + mobile article navbar

--- files/article.js
+++ files/article.js
@@ -2,3 +2,16 @@ let backToCategory = (new URL(window.location)).searchParams.get("backToCategory
 $(".arrow-button").removeAttr("onclick").attr("href", 
 	`${location.origin}/?type=${backToCategory || "new"}`
 );
+
+if ($(".navbar-nav").length == 0) {
+	$(`
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+					<a class="nav-link" href="/login">Login</a>
+			</li>
+			<li class="nav-item">
+					<a class="nav-link" href="/register/school-list/e9a?backTo=/">Register</a>
+			</li>
+		</ul>	
+	`).appendTo("#mySidepanel"); // lmao why is it called mySidepanel; 
+}
