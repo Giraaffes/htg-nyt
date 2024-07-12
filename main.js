96e2ae5087d610078216e95d1d8743616ad9328f
Marcus <marcusfaerk@gmail.com>
2024-07-12 16:06:01 +0200

    debugging github webhook

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ modules.register("thumbnails");
 
 // (_) Github webhook
 server.post("/github-push", (req, res) => {
+	console.log(req.headers, req.headers["x-github-hook-id"], config.githubWebhookId)
 	if (!req.headers["x-github-hook-id"] == config.githubWebhookId) return;
 	res.status(200).end();
 
