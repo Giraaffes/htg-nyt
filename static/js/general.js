b62733bc715869d8ed0851c9b1196694a6084273
Marcus <marcusfaerk@gmail.com>
2024-09-03 18:46:20 +0200

    "foredrag" tag

--- static/js/general.js
+++ static/js/general.js
@@ -184,5 +184,7 @@ const tags = [
 		oldName: "reporterne", name: "Reportage", uuid: "f03a2432-b51a-11ee-8448-1f36d397d0e6"
 	}, {
 		oldName: "lækkerier", name: "Opskrift", uuid: "9c56b2dc-afa6-11ee-af85-0b17f51c8368"
+	}, {
+		oldName: "talk", name: "Foredrag", uuid: "57f271f4-e5ec-11ee-83f8-87a53124b192"
 	}
 ];
