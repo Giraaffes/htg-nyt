34ce016431ab2c80ecc476d80955712fa699e633
Giraaffes <marcusfaerk@gmail.com>
2024-09-29 17:29:48 +0200

    removed shuffling

--- modules/general_category.js
+++ modules/general_category.js
@@ -2,29 +2,27 @@ const Module = require("../modules.js").Module;
 const mdl = module.exports = new Module();
 
 
-const categories = {
-	aktuelt: "11edfb97-c5d6-bab2-a165-31ad61c0f3b8",
-	hygge: "11eeab1d-d0b4-4284-9ea8-dd9b09e4a5f2",
-	fagligt: "11edfed6-b580-e5a6-9071-2bb1f6a2097f",
-	activities: "11edf97d-436a-5cb2-801f-7963935a19ec"
-};
+const acitivitesCtgUuid = "11edf97d-436a-5cb2-801f-7963935a19ec";
 
 function selectGeneralArticles(articles) {
 	let articlesNew10 = articles.filter(
-		a => a.category != categories.activities
+		a => a.category != acitivitesCtgUuid
 	).sort((a1, a2) => a2.date - a1.date).slice(0, 10);
 
-	let articlesShuffled = [];
-	let shuffleCtgOrder = [...new Set(articlesNew10.map(a => a.category))];
-	console.log(shuffleCtgOrder);
-	while (articlesShuffled.length < 10) {
-		for (let ctg of shuffleCtgOrder) {
-			let i = articlesNew10.findIndex(a => a.category == ctg);
-			if (i != -1) articlesShuffled.push(...articlesNew10.splice(i, 1));
-		}
-	}
+	// I think it's better not to shuffle
+	// let articlesShuffled = [];
+	// let shuffleCtgOrder = [...new Set(articlesNew10.map(a => a.category))];
+	// console.log(shuffleCtgOrder);
+	// while (articlesShuffled.length < 10) {
+	// 	for (let ctg of shuffleCtgOrder) {
+	// 		let i = articlesNew10.findIndex(a => a.category == ctg);
+	// 		if (i != -1) articlesShuffled.push(...articlesNew10.splice(i, 1));
+	// 	}
+	// }
 
-	return articlesShuffled;
+	// return articlesShuffled;
+
+	return articlesNew10;
 }
 
 mdl.hook("GET", "/", async (database, req, $) => {
